46ac95d97746cd75b8a86f8669256980db02ab00
Emine Bernardone <emine.bernardone@protonmail.com>
2021-07-22 15:06:36 +0200

    Refactored imported dll (dtos)

--- Pages/Exercises.cshtml.cs
+++ Pages/Exercises.cshtml.cs
@@ -5,7 +5,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using CalisthenicsAPI.Dtos;
+using CalisthenicsDtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
