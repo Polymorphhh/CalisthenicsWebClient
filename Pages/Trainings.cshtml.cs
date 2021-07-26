33327b6a812739173afc1d61799637c8e056f1fb
Emine Bernardone <emine.bernardone@protonmail.com>
2021-07-26 17:51:36 +0200

    Added glyphicons. Added trainings view.

--- Pages/Trainings.cshtml.cs
+++ Pages/Trainings.cshtml.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using CalisthenicsDtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +12,34 @@ namespace CalisthenicsWebClient.Pages
 {
     public class TrainingsModel : PageModel
     {
-        public void OnGet()
+        private readonly IHttpClientFactory _clientFactory;
+
+        public IEnumerable<TrainingReadDto> Trainings { get; private set; }
+
+        public bool GetTrainingsError { get; private set; }
+
+        public TrainingsModel(IHttpClientFactory clientFactory)
         {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task OnGet()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "trainings");
+            var client = _clientFactory.CreateClient("calisthenics");
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                Trainings = await JsonSerializer.DeserializeAsync
+                    <IEnumerable<TrainingReadDto>>(responseStream);
+            }
+            else
+            {
+                GetTrainingsError = true;
+                Trainings = Array.Empty<TrainingReadDto>();
+            }
         }
     }
 }
